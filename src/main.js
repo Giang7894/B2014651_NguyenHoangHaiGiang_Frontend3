c7600b500da90bba5fb6dc06c8a149d0e07c84d4
Giang7894 <giangb2014651@student.ctu.edu.vn>
2024-03-25 15:01:05 +0700

    Tao lop dich vu lay du lieu tu server

--- src/main.js
+++ src/main.js
@@ -2,5 +2,7 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
+import "@fortawesome/fontawesome-free/css/all.min.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 createApp(App).mount('#app')
