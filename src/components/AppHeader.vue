018320cc7f2f40a469dc11776be0d207659f99f5
Giang7894 <giangb2014651@student.ctu.edu.vn>
2024-03-26 16:40:50 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav class="navbar navbar-expand navbar-dark bg-dark">
+        <a href="/" class="navbar-brand">Ứng dụng quản lý danh bạ
+        </a>
+        <div class="mr-auto navbar-nav">
+            <ul>
+                <li class="nav-item">
+                    <router-link :to="{name: 'contactbook'}" class="nav-link">
+                        Danh bạ
+                        <i class="fas fa-address-book"></i>
+                    </router-link>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</template>
